55dd57353b69218cf6bd04db5cc2775a91919f8f
veera <veeraveerendra1999@gmail.com>
2024-03-23 15:23:29 +0530

    added some code

---
+++ sbi.py
@@ -0,0 +1,18 @@
+balance=1000
+pin=4488
+
+print("Welcome to SBI ATM")
+print("Insert to your card")
+
+pinn0=int(input("enter the pin"))
+if pin==pin:
+    print("Welcome Veera")
+    print("select the option")
+    withdrawlamount=int(input("enter the withdrawl amount"))
+    if withdrawlamount<=balance:
+        print("kirkirkir")
+    else:
+        print("insufficient funds")
+    else:
+        print("pin is wrong")
+
